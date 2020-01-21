1d30db31aa6926038eb2a77262bfeb0bcdb98fb6
huaxinrui <huaxinrui@bytedance.com>
2020-01-21 18:07:22 +0800

    xx

--- core/platform/routers.go
+++ core/platform/routers.go
@@ -28,6 +28,8 @@ func (r *Router) addCommon() {
 	router.GET("/conditions", control.PredictControlGlobal.GetConditions)
 	router.GET("/high_conditions", control.PredictControlGlobal.GetHighConditions)
 	router.GET("/query_list", control.PredictControlGlobal.GetQueryList)
+	router.GET("/share", control.UserControlGlobal.GetWxSign)
+
 }
 
 func (r *Router) addRouters() {
